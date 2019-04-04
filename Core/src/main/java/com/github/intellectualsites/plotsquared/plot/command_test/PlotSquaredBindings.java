96643ab9e6b1a016e35e6c5adbd68ea731e5d03b
Jesse Boyd <jessepaleg@gmail.com>
2019-04-05 00:57:46 +1100

    PlotPlayer binding

--- Core/src/main/java/com/github/intellectualsites/plotsquared/plot/command_test/PlotSquaredBindings.java
+++ Core/src/main/java/com/github/intellectualsites/plotsquared/plot/command_test/PlotSquaredBindings.java
@@ -1,10 +1,23 @@
 package com.github.intellectualsites.plotsquared.plot.command_test;
 
 import com.github.intellectualsites.plotsquared.plot.PlotSquared;
+import com.github.intellectualsites.plotsquared.plot.object.PlotPlayer;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.worldedit.extension.platform.Actor;
+import com.sk89q.worldedit.util.command.parametric.ArgumentStack;
 import com.sk89q.worldedit.util.command.parametric.Binding;
+import com.sk89q.worldedit.util.command.parametric.BindingBehavior;
 import com.sk89q.worldedit.util.command.parametric.BindingHelper;
+import com.sk89q.worldedit.util.command.parametric.BindingMatch;
 
 public class PlotSquaredBindings extends BindingHelper {
     public PlotSquaredBindings(PlotSquared ps) {
+    }
+
+    @BindingMatch(type = PlotPlayer.class,
+            behavior = BindingBehavior.PROVIDES)
+    public PlotPlayer getCommandContext(ArgumentStack context) {
+        Actor sender = context.getContext().getLocals().get(Actor.class);
+        return PlotPlayer.wrap(sender.getName());
     }
 }
