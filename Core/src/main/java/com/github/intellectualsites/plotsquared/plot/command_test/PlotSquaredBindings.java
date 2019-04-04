bd95be2131f878acaffeb1b153414cb1de30aa18
Jesse Boyd <jessepaleg@gmail.com>
2019-04-05 01:00:26 +1100

    add plot binding

--- Core/src/main/java/com/github/intellectualsites/plotsquared/plot/command_test/PlotSquaredBindings.java
+++ Core/src/main/java/com/github/intellectualsites/plotsquared/plot/command_test/PlotSquaredBindings.java
@@ -1,7 +1,9 @@
 package com.github.intellectualsites.plotsquared.plot.command_test;
 
 import com.github.intellectualsites.plotsquared.plot.PlotSquared;
+import com.github.intellectualsites.plotsquared.plot.object.Plot;
 import com.github.intellectualsites.plotsquared.plot.object.PlotPlayer;
+import com.github.intellectualsites.plotsquared.plot.util.MainUtil;
 import com.sk89q.minecraft.util.commands.CommandContext;
 import com.sk89q.worldedit.extension.platform.Actor;
 import com.sk89q.worldedit.util.command.parametric.ArgumentStack;
@@ -9,6 +11,9 @@ import com.sk89q.worldedit.util.command.parametric.Binding;
 import com.sk89q.worldedit.util.command.parametric.BindingBehavior;
 import com.sk89q.worldedit.util.command.parametric.BindingHelper;
 import com.sk89q.worldedit.util.command.parametric.BindingMatch;
+import com.sk89q.worldedit.util.command.parametric.ParameterException;
+
+import java.lang.annotation.Annotation;
 
 public class PlotSquaredBindings extends BindingHelper {
     public PlotSquaredBindings(PlotSquared ps) {
@@ -16,8 +21,23 @@ public class PlotSquaredBindings extends BindingHelper {
 
     @BindingMatch(type = PlotPlayer.class,
             behavior = BindingBehavior.PROVIDES)
-    public PlotPlayer getCommandContext(ArgumentStack context) {
+    public PlotPlayer getPlayer(ArgumentStack context) {
         Actor sender = context.getContext().getLocals().get(Actor.class);
         return PlotPlayer.wrap(sender.getName());
+    }
+
+    @BindingMatch(
+            type = Plot.class,
+            behavior = BindingBehavior.CONSUMES,
+            consumedCount = 1)
+    public Plot getCommandContext(ArgumentStack context) throws ParameterException {
+        PlotPlayer plr = getPlayer(context);
+        String input = context.next();
+        switch (input) {
+            case "me":
+                return plr.getCurrentPlot();
+            default:
+                return MainUtil.getPlotFromString(plr, input, false);
+        }
     }
 }
